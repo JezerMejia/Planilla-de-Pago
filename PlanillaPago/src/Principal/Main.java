81a5cfefe5eb425df912c152d3e914f6a698dcf9
leocnavarrete3@gmail.com <leocnavarrete3@gmail.com>
2021-11-23 20:04:30 -0600

    Actualizacion, se agrego la beta del login

--- PlanillaPago/src/Principal/Main.java
+++ PlanillaPago/src/Principal/Main.java
@@ -3,19 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package planillapago;
+package Principal;
+
+import Interfaz.InicioDeSesion;
 
 /**
  *
  * @author jezer
  */
-public class PlanillaPago {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        
     }
     
 }
