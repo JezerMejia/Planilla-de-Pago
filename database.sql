6708a120d49b04d951f97b73d1dfdfc71793caf4
JezerM <amyuki4@icloud.com>
2021-11-25 13:47:23 -0600

    Base de datos en sql añadido

---
+++ database.sql
@@ -0,0 +1,45 @@
+-- Se crea la base de datos
+CREATE DATABASE PlanillaPagos
+GO
+
+-- Se usa PlanillaPagos
+USE PlanillaPagos
+GO
+
+-- Se crean las tablas
+CREATE TABLE Empleados
+(
+    id int NOT NULL primary key,
+    nombre nvarchar(60) NOT NULL,
+    apellido nvarchar(60) NULL,
+    privilegios int NOT NULL,
+    cargo nvarchar(30) NULL,
+)
+
+CREATE TABLE Adelantos
+(
+    num int IDENTITY(1, 1) NOT NULL primary key,
+    aceptado bit NOT NULL,
+    adelanto float NOT NULL,
+    fecha date NOT NULL,
+    justificación nvarchar(255) NOT NULL,
+    idEmpleado int NOT NULL foreign key references Empleados(id),
+)
+
+CREATE TABLE Pagos
+(
+    num int IDENTITY(1, 1) NOT NULL primary key,
+    monto float NOT NULL,
+    fecha date NOT NULL,
+    idEmpleado int NOT NULL foreign key references Empleados(id),
+)
+
+CREATE TABLE Asistencias
+(
+    num int IDENTITY(1, 1) NOT NULL primary key,
+    asistencia bit NOT NULL,
+    fecha date NOT NULL,
+    idEmpleado int NOT NULL foreign key references Empleados(id)
+)
+
+GO
